33ecab6e36b9e6fe564799ec006b93c1daa3cb03
Kuber Karki <karki.kuber@gmail.com>
2016-09-27 17:06:54 +0545

    homepage fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -112,7 +112,7 @@ Home
                     @foreach($tabs_categories as $tab)
                         <div class="tab-pane fade {{ $tab['class']!=''?'in active':'' }}" id="tab-{{ $tab['id'] }}">
                             <div class="row" data-gutter="15">
-                            @if(isset($tab['products']))
+                            @if(array_key_exists('products', $$tab))
                             @foreach($tab['products'] as $product)
                                 <div class="col-md-2">
                                     <div class="product product-sm ">
